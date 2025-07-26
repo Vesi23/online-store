522d726223655fde4f4bd705e7e25f7d84de9e21
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-26 17:17:16 +0300

    refactor Header component for improved navigation and styling

--- online-store/src/components/Header/Header.tsx
+++ online-store/src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
+import './Header.css';
+
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header-css">
       <img src="/images/logo.JPG" alt="Logo" className="logo" />
-      <nav className="nav">
-        <ul>
-            <li><a href="/home">Home</a></li>
-          <li><a href="/">Контакти</a></li>
-          <li><a href="/profile">Свържи се с нас</a></li>
-          <li><a href="/products">Продукти</a></li>
+
+      <nav className="nav nav-css">
+        <ul className="menu">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/">Shop</a></li>
+          <li><a href="/about">About us</a></li>
         </ul>
       </nav>
     </header>
