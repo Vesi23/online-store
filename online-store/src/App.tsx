23c7fdd70db84333702eab101262010a80b445d4
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-26 22:54:59 +0300

    add Home route to App component for navigation

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import About from './views/About/About';
+import Home from './views/Home/Home';
 import WhatsAppButton from './components/WhatsApp-btn/WhatsApp-btn';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <WhatsAppButton />
         <Routes>
           <Route path="/about" element={<About/>} />
-
+<Route path='/home' element={<Home />} />
         </Routes>
         <Footer /> 
       </BrowserRouter>
