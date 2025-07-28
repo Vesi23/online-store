303af1735373f6eb585fba301fd8dea3008ca6ad
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-28 21:43:16 +0300

    update routing in App component to include Shop page and set Home as default route

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,24 +1,28 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import About from './views/About/About';
 import Home from './views/Home/Home';
 import WhatsAppButton from './components/WhatsApp-btn/WhatsApp-btn';
+import Shop from './views/Shop/Shop';
 
 function App() {
 
   return (
     // <AppContext.Provider value={{ ...context, setContext }}>
-      <BrowserRouter>
-        <Header />
-        <WhatsAppButton />
-        <Routes>
-          <Route path="/about" element={<About/>} />
-<Route path='/home' element={<Home />} />
-        </Routes>
-        <Footer /> 
-      </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <WhatsAppButton />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/shop' element={<Shop />} />
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
 
   );
 }
