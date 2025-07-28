a2bea190bb1575311be8490aae46e3c8e535d72f
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-28 21:44:06 +0300

    add Shop component with initial content and structure

---
+++ online-store/src/views/Shop/Shop.tsx
@@ -0,0 +1,11 @@
+const Shop = () => {
+    return (
+        <div className="shop">
+            <h1>Welcome to the Online Store</h1>
+            <p>Explore our wide range of products!</p>
+            {/* Additional shop content can be added here */}
+        </div>
+    );
+};
+
+export default Shop;
