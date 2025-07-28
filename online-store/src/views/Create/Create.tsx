985ddad0355bd7d871a552835a5f75149e5e0aa2
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-28 21:44:11 +0300

    add Create component with initial structure for new item creation

---
+++ online-store/src/views/Create/Create.tsx
@@ -0,0 +1,10 @@
+const Create = () => {
+    return (
+        <div>
+        <h1>Create New Item</h1>
+        {/* Form for creating a new item will go here */}
+        </div>
+    );
+};
+
+export default Create;
