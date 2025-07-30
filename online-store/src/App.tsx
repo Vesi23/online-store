9b19d5096962e36771c72a8f98144cfa13db7d82
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-30 20:47:32 +0300

    add Create route: include route for Create view in the application

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -4,13 +4,13 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import About from './views/About/About';
 import Home from './views/Home/Home';
+import Create from './views/Create/Create';
 import WhatsAppButton from './components/WhatsApp-btn/WhatsApp-btn';
 import Shop from './views/Shop/Shop';
 
 function App() {
 
   return (
-    // <AppContext.Provider value={{ ...context, setContext }}>
     <BrowserRouter>
       <Header />
       <WhatsAppButton />
@@ -19,7 +19,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path='/shop' element={<Shop />} />
-
+        <Route path="/create" element={<Create />} />
       </Routes>
       <Footer />
     </BrowserRouter>
