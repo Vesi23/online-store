09f7dee3a81ded9a11f1c84f8687fbb0ef6b8ee3
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-07-30 20:50:45 +0300

    refactor getAllProducts: remove unused properties from product mapping

--- online-store/src/service/product.ts
+++ online-store/src/service/product.ts
@@ -21,11 +21,7 @@ export const getAllProducts = async (search: string) => {
     const posts = Object.keys(snapshot.val()).map(key => ({
         id: key,
         ...snapshot.val()[key],
-        // createdOn: new Date(snapshot.val()[key].createdOn).toString(),
-        // likedBy: snapshot.val()[key].likedBy ? Object.keys(snapshot.val()[key].likedBy) : [],
         imageUrl: snapshot.val().imageUrl,
-        // pagesRead: snapshot.val()[key].pagesRead, // updated this line
-    // totalPages: snapshot.val()[key].totalPages, // updated this line
     }))
         .filter(p => p.title?.toLowerCase().includes(search.toLowerCase()));
 
