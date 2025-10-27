09444b82dfd7447590e246b911f7e2c448952fd3
Veselina Mancheva <veselina.mancheva4@gmail.com>
2025-10-27 09:46:39 +0200

    Update default WhatsApp message to Bulgarian for better localization

--- online-store/src/components/WhatsApp-btn/WhatsApp-btn.tsx
+++ online-store/src/components/WhatsApp-btn/WhatsApp-btn.tsx
@@ -1,7 +1,7 @@
 const WhatsAppButton = () => {
     const handleClick = () => {
         const phoneNumber = "359882042698"; // Replace with your phone number
-        const message = "Hello, I need assistance!"; // Default message
+        const message = "Здравейте, имам нужда от консултация."; // Default message
         const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
         window.open(url, "_blank");
     };
